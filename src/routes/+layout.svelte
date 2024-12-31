c98ad86cda6fbddce724c4042af79a28f9012eed
Igor-Ratajczak <betelgeza6@gmail.com>
2024-12-31 14:31:14 +0100

    feat: add PWA support with service workers and manifest with icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,34 @@
 <script lang="ts">
 	import '../app.less';
+	import { pwaInfo } from 'virtual:pwa-info';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		if (pwaInfo) {
+			const { registerSW } = await import('virtual:pwa-register');
+			registerSW({
+				immediate: true,
+				onRegisteredSW(swUrl, r) {
+					r && setInterval(() => {
+						console.log('Checking for sw update');
+						r.update();
+					}, 20000);
+					console.log(`SW Registered: ${swUrl}`);
+				},
+				onRegisterError(error: any) {
+					console.log('SW registration error', error);
+				}
+			});
+		}
+	});
+
+	let webManifestLink = $state(pwaInfo ? pwaInfo.webManifest.linkTag : '');
+
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	{@html webManifestLink}
+</svelte:head>
 
 {@render children()}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,69 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			base: './',
+			strategies: 'generateSW',
+			registerType: 'autoUpdate',
+			injectRegister: 'inline',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+			workbox: { globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,webmanifest}', 'prerendered/**/*.html'] },
+			manifest: {
+				name: 'Tic Tac Toe 3D',
+				short_name: 'Tic Tac Toe 3D',
+				description: 'Play Tic Tac Toe in a new dimension! A fun 3D twist on the classic game.',
+				id: '/',
+				start_url: './',
+				display: 'standalone',
+				background_color: '#0000FF',
+				theme_color: '#800080',
+				icons: [
+					{
+						src: '/web-app-manifest-144x144.png',
+						sizes: '144x144',
+						type: 'image/png',
+						purpose: 'any'
+					}, {
+						src: '/web-app-manifest-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'any'
+					}, {
+						src: '/web-app-manifest-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any'
+					}, {
+						src: '/web-app-manifest-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'maskable'
+					}, {
+						src: '/web-app-manifest-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable'
+					}
+				],
+				screenshots: [
+					{
+						src: 'web-app-manifest-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						form_factor: 'narrow'
+					},
+					{
+						src: 'web-app-manifest-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						form_factor: 'wide'
+					}
+				]
+			}
+		})
+	]
 });
