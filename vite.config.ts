28d13f9cc9b08fabebcdf9245249b7e227250975
Igor-Ratajczak <betelgeza6@gmail.com>
2024-12-31 14:56:28 +0100

    Fix: PWA not work offline, not found icons

--- vite.config.ts
+++ vite.config.ts
@@ -16,34 +16,34 @@ export default defineConfig({
 				name: 'Tic Tac Toe 3D',
 				short_name: 'Tic Tac Toe 3D',
 				description: 'Play Tic Tac Toe in a new dimension! A fun 3D twist on the classic game.',
-				id: '/',
+				id: './',
 				start_url: './',
 				display: 'standalone',
 				background_color: '#0000FF',
 				theme_color: '#800080',
 				icons: [
 					{
-						src: '/web-app-manifest-144x144.png',
+						src: 'web-app-manifest-144x144.png',
 						sizes: '144x144',
 						type: 'image/png',
 						purpose: 'any'
 					}, {
-						src: '/web-app-manifest-192x192.png',
+						src: 'web-app-manifest-192x192.png',
 						sizes: '192x192',
 						type: 'image/png',
 						purpose: 'any'
 					}, {
-						src: '/web-app-manifest-512x512.png',
+						src: 'web-app-manifest-512x512.png',
 						sizes: '512x512',
 						type: 'image/png',
 						purpose: 'any'
 					}, {
-						src: '/web-app-manifest-192x192.png',
+						src: 'web-app-manifest-192x192.png',
 						sizes: '192x192',
 						type: 'image/png',
 						purpose: 'maskable'
 					}, {
-						src: '/web-app-manifest-512x512.png',
+						src: 'web-app-manifest-512x512.png',
 						sizes: '512x512',
 						type: 'image/png',
 						purpose: 'maskable'
